fad7c283632665afc70f4b99e1133c6b8a85a141
Viktor H. Morales <viktorhugomorales@gmail.com>
2021-03-15 23:15:40 -0300

    Updated options on activation

--- includes/class-vhm-share-buttons-activator.php
+++ includes/class-vhm-share-buttons-activator.php
@@ -51,8 +51,9 @@ class Vhm_Share_Buttons_Activator {
         
 		// Add default options
 		update_option(self::$option_name . '_active', 'on');
-		update_option(self::$option_name . '_applications', array('facebook','twitter','google','whatsapp','telegram','link'));
+		update_option(self::$option_name . '_applications', array('facebook','twitter','whatsapp','telegram','link'));
 		update_option(self::$option_name . '_source', array('post','page'));
+		update_option(self::$option_name . '_display', 'after_content');
 	}
 
 }
